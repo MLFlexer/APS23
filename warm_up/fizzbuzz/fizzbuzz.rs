5deadfbb795eb4070564796d00e15c6e86fdbfb2
MLFlexer <malthemlarsen@gmail.com>
2023-01-26 21:32:56 +0100

    added fizz buzz

---
+++ warm_up/fizzbuzz/fizzbuzz.rs
@@ -0,0 +1,29 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let stdin = io::stdin();
+    for line in stdin.lock().lines().map(|l| l.unwrap()) {
+        let nums: Vec<i64> = line.split_whitespace()
+            .map(|num| num.parse().unwrap())
+            .collect();
+        let x = nums[0];
+        let y = nums[1];
+        let n = nums[2];
+
+        fizz_buzz(x, y, n)
+    }
+}
+
+fn fizz_buzz(x: i64, y: i64, n: i64) {
+    for i in 1..n + 1 {
+        if i % x == 0 && i % y == 0 {
+            println!("FizzBuzz");
+        } else if i % x == 0 {
+            println!("Fizz");
+        } else if i % y == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", i);
+        }
+    }
+}
