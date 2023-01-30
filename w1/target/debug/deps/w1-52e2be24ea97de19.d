8b580b1c9234e2c8bcef0997321584efbc70cb61
MLFlexer <malthemlarsen@gmail.com>
2023-01-30 21:43:43 +0100

    changed structure and finished warmup

--- w1/target/debug/deps/w1-52e2be24ea97de19.d
+++ w1/target/debug/deps/w1-52e2be24ea97de19.d
@@ -1,5 +1,6 @@
-/home/hotdog2c/Desktop/APS23/w1/target/debug/deps/w1-52e2be24ea97de19.rmeta: src/main.rs
+/home/hotdog2c/Desktop/APS23/w1/target/debug/deps/w1-52e2be24ea97de19.rmeta: src/main.rs src/server.rs
 
-/home/hotdog2c/Desktop/APS23/w1/target/debug/deps/w1-52e2be24ea97de19.d: src/main.rs
+/home/hotdog2c/Desktop/APS23/w1/target/debug/deps/w1-52e2be24ea97de19.d: src/main.rs src/server.rs
 
 src/main.rs:
+src/server.rs:
--- w1/target/debug/deps/w1-a89c4169ab2bb661.d
+++ w1/target/debug/deps/w1-a89c4169ab2bb661.d
@@ -1,5 +1,6 @@
-/home/hotdog2c/Desktop/APS23/w1/target/debug/deps/w1-a89c4169ab2bb661.rmeta: src/main.rs
+/home/hotdog2c/Desktop/APS23/w1/target/debug/deps/w1-a89c4169ab2bb661.rmeta: src/main.rs src/server.rs
 
-/home/hotdog2c/Desktop/APS23/w1/target/debug/deps/w1-a89c4169ab2bb661.d: src/main.rs
+/home/hotdog2c/Desktop/APS23/w1/target/debug/deps/w1-a89c4169ab2bb661.d: src/main.rs src/server.rs
 
 src/main.rs:
+src/server.rs:
